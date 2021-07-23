4e6a1d07003c0ee8f6a70416652afed7685ed644
mauratee <mauraethompson@gmail.com>
2021-07-23 19:58:03 +0000

    Create Todo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import TodoList from "./TodoList";
+import { useState } from 'react'
 
 function App() {
+  // set use state for TodoList
+  const [todos, setTodos] = useState(['ToDo 1', 'ToDo 2'])
   return (
     // create and return component as jsx wrapped in fragment
     <>
-      <TodoList />
+      <TodoList todos={todos} />
       <input type="text" />
       <button>Add ToDo</button>
       <button>Clear Completed</button>
