610ece8fbcfaf6597550d3ed21d8083d98134f20
mauratee <mauraethompson@gmail.com>
2021-07-23 15:57:30 +0000

    Add basic button and other html to App function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import TodoList from "./TodoList";
 
 function App() {
   return (
-    // create and return component as jsx
-    <TodoList />
+    // create and return component as jsx wrapped in fragment
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add ToDo</button>
+      <button>Clear Completed</button>
+      <div>0 left to do</div>
+    </>
   )
 }
 
