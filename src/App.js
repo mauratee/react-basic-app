6bbf2eb4b6cb80794ea797f788533e9d70a2c709
mauratee <mauraethompson@gmail.com>
2021-07-23 21:16:15 +0000

    Finish all functions and functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
 import TodoList from "./TodoList";
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   // set use state for TodoList
-  const [todos, setTodos] = useState(['ToDo 1', 'ToDo 2'])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem
+      (LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo (e) {
+    const name = todoNameRef.current.value
+    if (name === ' ') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete:false}]
+    })
+    todoNameRef.current.value = null
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
     // create and return component as jsx wrapped in fragment
     <>
-      <TodoList todos={todos} />
-      <input type="text" />
-      <button>Add ToDo</button>
-      <button>Clear Completed</button>
-      <div>0 left to do</div>
+      <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add ToDo</button>
+      <button onClick={handleClearTodos}>Clear Completed</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </>
   )
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-export default function Todo({ todo }) {
+export default function Todo({ todo, toggleTodo }) {
+    function handleTodoclick()  {
+        toggleTodo(todo.id)
+    }
+    
     return (
         <div>
-            
+            <label>
+                <input type="checkbox" checked={todo.complete} onChange={handleTodoclick}/>
+                {todo.name}
+            </label>
         </div>
     )
 }
